63d2a8746719eff44e41d87bf74bbb9f9a66d2c0
Verseoe <111694103+Verseoe@users.noreply.github.com>
2022-08-23 16:17:11 +0300

    Create my-Age-in-dog-Year.js
    my code here helps you convert yourself to a dog if you are interested

---
+++ my-Age-in-dog-Year.js
@@ -0,0 +1,16 @@
+let myAge = 18;
+//putting my Age in a VAR
+let earlyYears = 2;
+//First two years in a dog life span is diffr so i put it in a var
+earlyYears = earlyYears * 10.5
+//the first to years in a dog life span counted as 10.5 years in a human span so a should make it equal
+let laterYears = myAge - 2 ;
+//i subtracted the first to years and attached it to a new var
+laterYears = laterYears * 4;
+// cuase the laters year counted as 4 in human span
+//console.log(earlyYears);
+//console.log(laterYears);
+//
+let myAgeInDogYears = ( earlyYears + laterYears);
+let myName = "naif ahmad".toUpperCase();
+console.log('My name is '+ myName + "." + ` i am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years .` )
